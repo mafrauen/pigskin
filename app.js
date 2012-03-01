d7c9d2d3658de1d14bc2372dc0100e9f350f06da
Michael Frauenholtz <mafrauen@gmail.com>
2012-02-29 21:32:33 -0600

    Create picks page

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/picks/', routes.picks);
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,23 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+var games = [
+{
+  favorite: "Iowa",
+  opponent: "Nebraska",
+  spread: 9.5,
+  home_favorite: true
+},
+{
+  favorite: "Alabama",
+  opponent: "LSU",
+  spread: 1,
+  home_favorite: true
+}];
+
+exports.picks = function(req, res){
+  res.render('picks', { title: 'Express',
+                        user: 'Mike',
+                        games: games})
+};
