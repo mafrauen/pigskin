9d14110ab7d9eb66af4ab44b1f163a19e175f763
Michael Frauenholtz <mafrauen@gmail.com>
2012-03-05 21:18:24 -0600

    Page to create new week POSTs weekly data to MongoDB

--- routes/index.js
+++ routes/index.js
@@ -45,10 +45,11 @@ exports.index = function(req, res){
 
 exports.picks = function(req, res){
   users.findOne({full_name:'Michael Frauenholtz'}, function(err, doc) {
-    weeks.findOne({number:1}, function(err, week) {
+    weeks.find({}).desc('number').limit(1).run(function(err, week_docs) {
+      console.log(week_docs);
       res.render('picks', { title: 'Pigskin Picks',
                             user: doc,
-                            week: week })
+                            week: week_docs[0] })
     });
   });
 };
@@ -74,11 +75,32 @@ exports.submit_picks = function(req, res){
 
 // GET new week
 exports.week_new = function(req, res) {
-  res.render('week', { title: 'Pigskin Picks' });
+  weeks.count({}, function(err, size) {
+    res.render('week', { title: 'Pigskin Picks',
+                         week_size: size });
+  });
 }
 
 // POST new week
 exports.week_create = function(req, res) {
-  console.log(req.body);
+  var week = new weeks();
+  week.number = req.body.week.number;
+
+  for (var i = 0; i< 10; i++) {
+    var game = {};
+    game.team_favorite = req.body.week.team_favorite[i];
+    game.team_opponent = req.body.week.team_opponent[i];
+    game.spread = req.body.week.spread[i];
+    game.is_favorite_home = req.body.week.home_team.indexOf((i+1).toString()) >= 0;
+    week.games.push(game);
+  }
+
+  week.tiebreaker_favorite = req.body.week.tiebreaker_favorite;
+  week.tiebreaker_opponent = req.body.week.tiebreaker_opponent;
+  week.tiebreaker_spread = req.body.week.tiebreaker_spread;
+  week.tiebreaker_home_favorite = req.body.week.tiebreaker_home_favorite;
+
+  week.save(function(err) {});
+
   res.redirect('back');
 }
